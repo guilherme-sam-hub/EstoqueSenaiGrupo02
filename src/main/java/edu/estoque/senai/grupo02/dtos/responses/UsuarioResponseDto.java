f5be6df613946d60e6d6b14c0d3b2da9fe4a7127
guilherme-sam-hub <gsamsin@gmail.com>
2026-08-14 21:56:47 -0300

    feat: construção do API para cadastro e validação do usuario repositorio, dto, service e controller

--- src/main/java/edu/estoque/senai/grupo02/dtos/responses/UsuarioResponseDto.java
+++ src/main/java/edu/estoque/senai/grupo02/dtos/responses/UsuarioResponseDto.java
@@ -1,4 +1,28 @@
 package edu.estoque.senai.grupo02.dtos.responses;
 
+import edu.estoque.senai.grupo02.entities.Usuario;
+
 public class UsuarioResponseDto {
+    public Long id;
+    public String nome;
+    public String email;
+
+    //Da entity para DTO (via construtor)
+    public UsuarioResponseDto(Usuario usuario) {
+        this.id = usuario.getId();
+        this.nome = usuario.getNome();
+        this.email = usuario.getEmail();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
--- src/main/java/edu/estoque/senai/grupo02/services/UsuarioService.java
+++ src/main/java/edu/estoque/senai/grupo02/services/UsuarioService.java
@@ -1,9 +1,47 @@
 package edu.estoque.senai.grupo02.services;
 
+import edu.estoque.senai.grupo02.dtos.requests.LoginRequestDto;
+import edu.estoque.senai.grupo02.dtos.requests.UsuarioRequestDto;
+import edu.estoque.senai.grupo02.entities.Usuario;
+import edu.estoque.senai.grupo02.repositories.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/usuarios")
+import java.util.Optional;
+
+@Service
 public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public Usuario salvar(UsuarioRequestDto dadosCadastro){
+        if (usuarioRepository.findByEmail(dadosCadastro.getEmail()).isPresent()) {
+            throw new RuntimeException("E-mail já cadastrado");
+        }
+        //Da DTO pra Entity
+        Usuario novoUsuario = new Usuario();
+        novoUsuario.setNome(dadosCadastro.getNome());
+        novoUsuario.setEmail(dadosCadastro.getEmail());
+        novoUsuario.setSenha(dadosCadastro.getSenha());
+
+        return usuarioRepository.save(novoUsuario);
+    }
+    public Usuario validarLogin(LoginRequestDto dadosLogin){
+        //busca no banco usuario<->email
+        Optional<Usuario> usuarioPossivel = usuarioRepository.findByEmail(dadosLogin.getEmail());
+
+        //se email existe
+        if (usuarioPossivel.isPresent()) {
+            Usuario usuario = usuarioPossivel.get();
+
+            // se senha digitada <-> senha no banco
+            if (usuario.getSenha().equals(dadosLogin.getSenha())) {
+            return usuario; //login
+            }
+        }
+        return null; //se falhar nas condicionais
+    }
 }
