724578b251d1e0f7d6e33c82983cd55c270093a1
guilherme-sam-hub <gsamsin@gmail.com>
2026-08-18 18:19:25 -0300

    fix: corrige pacote e formatação da entidade Produto

--- src/main/java/edu/estoque/senai/grupo02/entities/Produto.java
+++ src/main/java/edu/estoque/senai/grupo02/entities/Produto.java
@@ -1,11 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package estoque.entities;
+package edu.estoque.senai.grupo02.entities;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.math.BigDecimal;
+
 
 @Entity
 @Table (name = "produto")
