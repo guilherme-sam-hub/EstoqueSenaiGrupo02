54dd0652607ad6779d39ff8aec82597ce46f6dae
leticiafernandarezende-droid <leticiafernandarezende@gmail.com>
2026-08-18 16:32:10 -0300

    Add files via upload

--- src/main/java/edu/estoque/senai/grupo02/repositories/UsuarioRepository.java
+++ src/main/java/edu/estoque/senai/grupo02/repositories/UsuarioRepository.java
@@ -1,13 +1,15 @@
-package edu.estoque.senai.grupo02.repositories;
 
-import edu.estoque.senai.grupo02.entities.Usuario;
+
+
+package estoque.repositories;
+
+import estoque.entities.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
 
-@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    Optional<Usuario> findByEmail(String email);
 
+    Optional<Usuario> findByEmailAndSenha(String email, String senha);
 }
+
+
